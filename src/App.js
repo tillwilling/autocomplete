97ae8cd4e9a1c4dd517640befb984a17ddf8e12f
Till Willing <tillwilling@gmail.com>
2021-06-17 15:30:06 +0200

    edited some wordings from pokemon to game and added .env to hide key for issue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import "./App.css";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const Auto = () => {
+  const [display, setDisplay] = useState(false);
+  const [options, setOptions] = useState([]);
+  const [search, setSearch] = useState("");
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    const game = [];
+    const promises = new Array(20)
+      .fill()
+      .map((v, i) => fetch(`https://rawg.io/api/games?key=${API_KEY}${i + 1}`));
+    Promise.all(promises).then((gameArr) => {
+      return gameArr.map((value) =>
+        value.json().then(({ name }) => game.push({ name }))
+      );
+    });
+    setOptions(game);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const handleClickOutside = (event) => {
+    const { current: wrap } = wrapperRef;
+    if (wrap && !wrap.contains(event.target)) {
+      setDisplay(false);
+    }
+  };
+
+  const updatePokeDex = (poke) => {
+    setSearch(poke);
+    setDisplay(false);
+  };
+
+  return (
+    <div ref={wrapperRef} className="flex-container flex-column pos-rel">
+      <input
+        id="auto"
+        onClick={() => setDisplay(!display)}
+        placeholder="Type to search"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      {display && (
+        <div className="autoContainer">
+          {options
+            .filter(({ name }) => name.indexOf(search.toLowerCase()) > -1)
+            .map((value, i) => {
+              return (
+                <div
+                  onClick={() => updatePokeDex(value.name)}
+                  className="option"
+                  key={i}
+                  tabIndex="0"
+                >
+                  <span>{value.name}</span>
+                </div>
+              );
+            })}
+        </div>
+      )}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Custom AutoComplete React</h1>
+      <div className="logo"></div>
+      <div className="auto-container">
+        <Auto />
+      </div>
     </div>
   );
 }
